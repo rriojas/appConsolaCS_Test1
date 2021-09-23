05b1eb4b68b3af5cfce489d2084320548ecaa09e
AmadorLA <amadorlopezalejandro1@gmail.com>
2021-09-23 11:41:46 -0500

    cambio yo

--- appConsolaCS_Test1/Program.cs
+++ appConsolaCS_Test1/Program.cs
@@ -11,7 +11,7 @@ namespace appConsolaCS_Test1
     static void Main(string[] args)
     {
       Console.WriteLine("Hola ..");
-      Console.WriteLine("Mundo");
+      Console.WriteLine("Alejandro");
       Console.ReadKey();
     }
   }
